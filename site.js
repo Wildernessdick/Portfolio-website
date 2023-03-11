07f1e0eb1510f09d042bef1f85ce8513d8161de7
Simo <76773814+Hovari@users.noreply.github.com>
2023-03-11 11:54:48 +0200

    15 game has now close operation.

--- site.js
+++ site.js
@@ -1,91 +1,39 @@
-
-
-//// Make window spawn to random location on page load
+// Make window spawn to random location on page load
 window.addEventListener('load', function ()
 {
-
+    hideGame();
     const windowDiv = document.getElementById("window");
     windowDiv.style.top = "120px";
     windowDiv.style.left = "800px";
 
+    const windowDiv2 = document.getElementById("window2");
+    windowDiv2.style.top = "250px";
+    windowDiv2.style.left = "600px";
 
-    // var randomDiv = document.getElementById('window');
-    // var maxX = window.innerWidth - randomDiv.offsetWidth;
-    // var maxY = window.innerHeight - randomDiv.offsetHeight;
-    // var randomX = Math.floor(Math.random() * maxX);
-    // var randomY = Math.floor(Math.random() * maxY);
-    // randomDiv.style.top = randomY + 'px';
-    // randomDiv.style.left = randomX + 'px';
 });
 
 // Make the DIV element draggable:
 dragElement(document.getElementById("window"));
-
-function dragElement(elmnt)
-{
-    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-    if (document.getElementById(elmnt.id + "header"))
-    {
-        // if present, the header is where you move the DIV from:
-        document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
-    } else
-    {
-        // otherwise, move the DIV from anywhere inside the DIV:
-        elmnt.onmousedown = dragMouseDown;
-    }
-
-    function dragMouseDown(e)
-    {
-        e = e || window.event;
-        e.preventDefault();
-        // get the mouse cursor position at startup:
-        pos3 = e.clientX;
-        pos4 = e.clientY;
-        document.onmouseup = closeDragElement;
-        // call a function whenever the cursor moves:
-        document.onmousemove = elementDrag;
-    }
-
-    function elementDrag(e)
-    {
-        e = e || window.event;
-        e.preventDefault();
-        // calculate the new cursor position:
-        pos1 = pos3 - e.clientX;
-        pos2 = pos4 - e.clientY;
-        pos3 = e.clientX;
-        pos4 = e.clientY;
-        // set the element's new position:
-        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-    }
-
-    function closeDragElement()
-    {
-        // stop moving when mouse button is released:
-        document.onmouseup = null;
-        document.onmousemove = null;
-    }
-}
-
 dragElement(document.getElementById("window2"));
 
 function dragElement(elmnt)
 {
     var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+
+    // Check if the element has a header element to drag from, otherwise drag from anywhere
     if (document.getElementById(elmnt.id + "header"))
     {
-        // if present, the header is where you move the DIV from:
+        // If present, the header is where you move the DIV from:
         document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
     } else
     {
-        // otherwise, move the DIV from anywhere inside the DIV:
+        // Otherwise, move the DIV from anywhere inside the DIV:
         elmnt.onmousedown = dragMouseDown;
     }
 
     function dragMouseDown(e)
     {
-        e = e || window2.event;
+        e = e || window.event;
         e.preventDefault();
         // get the mouse cursor position at startup:
         pos3 = e.clientX;
@@ -97,7 +45,7 @@ function dragElement(elmnt)
 
     function elementDrag(e)
     {
-        e = e || window2.event;
+        e = e || window.event;
         e.preventDefault();
         // calculate the new cursor position:
         pos1 = pos3 - e.clientX;
@@ -128,3 +76,23 @@ function updateTime()
 
 updateTime();
 setInterval(updateTime, 1000);
+
+const startBtn = document.querySelector('.start');
+const menu = document.querySelector('.menu');
+
+startBtn.addEventListener('click', function ()
+{
+    menu.style.display = menu.style.display === 'none' ? 'block' : 'none';
+});
+
+function hideGame()
+{
+    var x = document.getElementById("window");
+    if (x.style.display === "none")
+    {
+        x.style.display = "block";
+    } else
+    {
+        x.style.display = "none";
+    }
+}
